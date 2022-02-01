a7196f083b455f88fba337392a6d3f34228966d8
vonbell <divonvbell@gmail.com>
2022-02-01 17:05:55 -0500

    recipes page form creates recipes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 //import { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
+import { useState, useEffect } from 'react';
 // import pages
 import About from "../pages/About";
 import Contact from "../pages/Contact";
@@ -12,9 +13,49 @@ import Videos from "../pages/Videos";
 // Update recipes using fetch pass as props into show by id route
 // Delete recipes using fetch pass as props into show by id route
 
-
-
 function Main() {
+    const [peopleRecipe, setPeopleRecipe] = useState(null);
+    // const URL = 'https://nom-nom-api-22.herokuapp.com/';
+    const URL = 'http://localhost:4000/recipes/';
+
+    const getPeopleRecipe = async() => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeopleRecipe(data);
+    };
+
+    const createPeopleRecipe = async(recipe) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/JSON"
+            },
+            body: JSON.stringify(recipe)
+        });
+        getPeopleRecipe();
+    };
+
+    const updatePeopleRecipe = async (recipe, id) => {
+        await fetch(URL + id, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "Application/json",
+          },
+          body: JSON.stringify(recipe),
+        });
+        getPeopleRecipe();
+    };
+
+    const deletePeopleRecipe = async id => {
+        await fetch(URL + id, {
+          method: "DELETE",
+        })
+        getPeopleRecipe();
+    };
+
+    useEffect(() => getPeopleRecipe(), []);
+
+
     return (
         <main className="main">
             <Switch>
@@ -22,9 +63,10 @@ function Main() {
                     <Home />
                 </Route>
                 <Route exact path="/recipes">
-                    <Recipes />
+                    <Recipes peopleRecipe={peopleRecipe} createPeopleRecipe={createPeopleRecipe} />
                 </Route>
                 {/* path to recipes id pass render props */}
+                {/* <Route path="/recipes/:id" peopleRecipe={peopleRecipe} updatePeopleRecipe={updatePeopleRecipe} deletePeopleRecipe={deletePeopleRecipe} {...rp} /> */}
                 <Route path="/recipes/:id" />
                 <Route exact path="/videos">
                     <Videos />
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,79 @@
+import { useState } from 'react';
+
+function Recipes(props) {
+    const [newForm, setNewForm] = useState({
+        name: "",
+        title: "",
+        image: "",
+        readyInMinutes: "",
+        instructions: "",
+    });
+
+    const handleChange = (evt) => {
+        setNewForm({ ...newForm, [evt.target.name]: evt.target.value }); 
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        props.createPeopleRecipe(newForm);
+        setNewForm({
+            name: "",
+            title: "",
+            image: "",
+            readyInMinutes: "",
+            instructions: "",
+        });
+    };
+
+    const loaded = () => {
+        return props.peopleRecipe.map((recipe) => (
+            <div key={recipe._id} className="recipe">
+                <h1> {recipe.title} </h1>
+                { recipe.image && <img src={recipe.image} alt={recipe.name} />}
+                <p> Ready in {recipe.readyInMinutes} minutes </p>
+                <p> Instructions: {recipe.instructions}</p>
+            </div>
+        ));
+    };
+    const loading = () => {
+        return <h1>Recipes...</h1>;
+    };
 
 
-function Recipes() {
     return (
         <div className="recipes">
-            RECIPES INDEX
+            <form className='index-form' onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={newForm.title}
+                    name="title"
+                    placeholder="title"
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    value={newForm.image}
+                    name="image"
+                    placeholder="image URL"
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    value={newForm.readyInMinutes}
+                    name="readyInMinutes"
+                    placeholder="readyInMinutes"
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    value={newForm.instructions}
+                    name="instructions"
+                    placeholder="instructions"
+                    onChange={handleChange}
+                /> <br />
+                <input type="submit" value="Create Recipe" />
+            </form>
+            {props.peopleRecipe ? loaded() : loading()}
         </div>
     );
 }
