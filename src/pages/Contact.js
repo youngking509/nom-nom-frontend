eaf778cac00c9905e7ad1345e40e7213c3b3f9ba
kingzy <kingzyv14@gmail.com>
2022-02-01 06:53:08 -0800

    about page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,12 +4,15 @@ function Contact() {
     return (
         <div>
        <h1>Contact us</h1>
-       Name: <input type="name" className="name" /> <br />
+       Name: <input type="name" className="name" />
        Email: <input type="email" className="email" /> <br />
        <textarea name="" id="" cols="30" rows="10" placeholder="Write your comment"></textarea>
        <button type="submit" class="btn-primary">Submit</button>
-
-
+       <p>
+       dazzo.chris@gmail.com <br />
+       kingzyv14@gmail.com <br />
+       divonvbell@gmail.com <br />
+       </p>
         </div>
         
     );
