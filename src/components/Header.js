352ccc00c718601acbf74acdf2056c6bf979d249
Christopher Dazzo <christopherdazzo@Christophers-MacBook-Air.local>
2022-01-31 19:40:25 -0600

    responsive nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo from "../NomNomSVG/logo-stacked.svg";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
+    const [showLinks, setShowLinks] = useState(false);
+
     return (
         <nav className="navBar">
             <div className="nav-left">
                 <Link to="/">
                     <img src={logo} alt="Logo" className="logo"/>
                 </Link>
-                <div className="nav-links">
+                <div className="nav-links" id={showLinks ? "hidden" : ""}>
                     <Link to="/recipes" className="recipes-link">
                         <div>RECIPES</div>
                     </Link>
@@ -26,6 +30,9 @@ function Header() {
                         <div>CONTACT</div>
                     </Link>
                 </div>
+                <button className="menuIcon" onClick={() => setShowLinks(!showLinks)}>
+                    <MenuIcon />
+                </button>
             </div>
             <div className="nav-right">
                 <TwitterIcon />
